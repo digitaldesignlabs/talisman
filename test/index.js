03b490654171be9061bf1fe688bddc31269551ed
David <david.hignett@digitaldesignlabs.com>
2016-04-01 17:30:25 +0100

    added more unit tests

--- test/index.js
+++ test/index.js
@@ -79,7 +79,7 @@ function contentParsingTest(test) {
 function templateInterfaceTest(test) {
     // Establish that the correct methods are exposed in the API
     const expectedTemplateMethods = [
-        "load",
+        "create",
         "debug",
         "showUndefined"
     ];
@@ -93,8 +93,8 @@ function templateInterfaceTest(test) {
 }
 
 template.debug(false);
-function invalidTemplateTest(test) {
-    const invalidTemplate = template.load('invalid');
+function testCreateInvalidTemplate(test) {
+    const invalidTemplate = template.create('invalid');
 
     // Rendering an invalid template will result in an error being sent to the browser
     let htmlResult = '';
@@ -113,7 +113,7 @@ function invalidTemplateTest(test) {
 const testFile = path.join(__dirname, "testTemplate.html");
 
 
-function validTemplateTest(test) {
+function testCreateValidTemplate(test) {
     // Establish that the correct methods are exposed within the context of the template
     const expectedContextMethods = [
         "load",
@@ -128,7 +128,7 @@ function validTemplateTest(test) {
     ];
 
     // Set template path to current directory so that we can correctly retrieve
-    const testTemplate = template.load(testFile);
+    const testTemplate = template.create(testFile);
 
     test.expect(expectedContextMethods.length + 1);
     expectedContextMethods.forEach(function (method) {
@@ -146,93 +146,179 @@ function validTemplateTest(test) {
         });
 }
 
-function testPage(test) {
+// Strings to be returned by test promises and streams.
+const returnStrings = {};
+returnStrings.testPromise1Text = "This text was returned from a promise";
+returnStrings.readableMessage = "This text was arrived via a Readable Stream.";
+returnStrings.duplexMessage = "This text was arrived via a Duplex Stream.";
+returnStrings.transformMessage = "This text was arrived via a Transform Stream.";
 
-    const testTemplate = template.load(testFile);
+// Variables of all types to be used in trying to break various functions and methods.
+const testVariables = {};
+testVariables.testString1 = "TestString";
+testVariables.testNumber1 = 7790.25;
+testVariables.testArray1 = ["Melons", "Apples", "Bananas", "Oranges", "Plums"];
+testVariables.testObject1 = {testKey1: "value1", testKey2: "value2"};
+const testObjectSize = Object.keys(testVariables.testObject1).length;
+testVariables.testReadableStream1 = new stream.Readable();
+testVariables.testDuplexStream1 = new stream.Duplex();
+testVariables.testTransformStream1 = new stream.Transform();
+testVariables.testPromise1 = new Promise(function (resolve) {
+    setTimeout(resolve, 500, returnStrings.testPromise1Text);
+});
+testVariables.testFunction1 = function () {
+    return "Function Returned";
+};
+const numberOfTestVariables = Object.keys(testVariables).length;
+// Set up _read properties on the streams
+const dummyReadMethod = function () {
+    // It is necessary to define a _read function for the stream, however in our implementation we are pushing
+    // items onto the stream directly so this function is not needed. If this function is not defined then you will
+    // be spammed with 'not implemented' errors.
+    return undefined;
+};
+testVariables.testReadableStream1._read = dummyReadMethod; // _read is a bad property name according to JSLint
+testVariables.testDuplexStream1._read = dummyReadMethod; // _read is a bad property name according to JSLint
+testVariables.testTransformStream1._read = dummyReadMethod; // _read is a bad property name according to JSLint
 
-    const testString1 = "Test String";
-    const testNumber1 = 7790.25;
-    const testArray1 = ["Melons", "Apples", "Bananas", "Oranges", "Plums"];
-    const testObject1 = {testKey1: "value1", testKey2: "value2"};
-    const testPromise1Text = "This text was returned from a promise";
-    const testPromise1 = new Promise(function (resolve) {
-        setTimeout(resolve, 500, testPromise1Text);
-    });
+// Send stuff through the streams and close them.
+testVariables.testReadableStream1.push(returnStrings.readableMessage);
+testVariables.testDuplexStream1.push(returnStrings.duplexMessage);
+testVariables.testTransformStream1.push(returnStrings.transformMessage);
+testVariables.testReadableStream1.push(null);
+testVariables.testDuplexStream1.push(null);
+testVariables.testTransformStream1.push(null);
 
-    const dummyReadMethod = function () {
-        // It is necessary to define a _read function for the stream, however in our implementation we are pushing
-        // items onto the stream directly so this function is not needed. If this function is not defined then you will
-        // be spammed with 'not implemented' errors.
-        return undefined;
-    };
-
-    const testReadableStream1 = new stream.Readable();
-    const readableMessage = "This text was arrived via a Readable Stream.";
-    testReadableStream1._read = dummyReadMethod; // _read is a bad property name according to JSLint
-    testReadableStream1.push(readableMessage);
-    testReadableStream1.push(null);
-    const testDuplexStream1 = new stream.Duplex();
-    const duplexMessage = "This text was arrived via a Duplex Stream.";
-    testDuplexStream1._read = dummyReadMethod; // _read is a bad property name according to JSLint
-    testDuplexStream1.push(duplexMessage);
-    testDuplexStream1.push(null);
-    const testTransformStream1 = new stream.Transform();
-    const transformMessage = "This text was arrived via a Transform Stream.";
-    testTransformStream1._read = dummyReadMethod; // _read is a bad property name according to JSLint
-    testTransformStream1.push(transformMessage);
-    testTransformStream1.push(null);
-
-    const testFunction1 = function () {
-        return "Function Returned";
-    };
-
-
-
-    /**
-    * set() function testing
-    */
-
-    const setErrors = [];
-
-    const trySetWithType = function (arg1, arg2) {
+// This function tests all variable types being entered into a specified function
+const tryAllVariableTypes1 = function (func, testInput) {
+    const errors = [];
+    const successes = [];
+    let total = 0;
+    const varKeys = Object.keys(testInput);
+    varKeys.forEach(function (key) {
+        total += 1;
+        const variable = testInput[key];
         try {
-            testTemplate.set(arg1, arg2);
-        } catch (ignore) {
-            setErrors.push([typeof arg1, arg2]);
+            func(variable);
+            successes.push(variable); // This line shouldn't be reached if there is an exception
+        } catch (err) {
+            errors.push(err);
         }
-    };
-    // try the set function with no parameters
-    trySetWithType();
-    // Test all variable types by trying to assign using one argument. Most should fail
-    trySetWithType(undefined);  // should fail
-    trySetWithType(null);  // should fail
-    trySetWithType(testString1); // should fail
-    trySetWithType(testNumber1); // should fail
-    trySetWithType(testArray1); // should fail
-    trySetWithType(testPromise1); // should fail
-    trySetWithType(testReadableStream1); // should fail
-    trySetWithType(testDuplexStream1); // should fail
-    trySetWithType(testTransformStream1); // should fail
-    trySetWithType(testFunction1); // should fail
-    trySetWithType(testObject1); // key:value pair object should succeed.
+    });
+    return {successes, errors, total};
+};
 
-    // Test all variable types by trying to assign using two arguments. All should succeed.
-    trySetWithType(undefined, "templateTestUndefined1");
-    trySetWithType(testString1, "templateTestString1");
-    trySetWithType(testNumber1, "templateTestNumber1");
-    trySetWithType(testArray1, "templateTestArray1");
-    trySetWithType(testObject1, "templateTestObject1");
-    trySetWithType(testPromise1, "templateTestPromise1");
-    trySetWithType(testReadableStream1, "templateTestReadableStream1");
-    trySetWithType(testDuplexStream1, "templateTestDuplexStream1");
-    trySetWithType(testTransformStream1, "templateTestTransformStream1");
-    trySetWithType(testFunction1, "templateTestFunction1");
+// This function tests a function that takes two parameters by trying to enter every type of variable into it.
+const tryAllVariableTypes2 = function (func, testInput) {
+    const errors = [];
+    const successes = [];
+    let total = 0;
+    const varKeys = Object.keys(testInput);
+    varKeys.forEach(function (key1) {
+        const variable1 = testInput[key1];
+        varKeys.forEach(function (key2) {
+            total += 1;
+            const variable2 = testInput[key2];
+            try {
+                func(variable1, variable2);
+                successes.push([typeof variable1, typeof variable2]); // This line shouldn't be reached if there is an exception
+            } catch (err) {
+                errors.push([typeof variable1, typeof variable2]);
+            }
+        });
+    });
+    return {successes, errors, total};
+};
+
+// template.load() can be called with one argument, a string representing a path to a file to load. The block name is
+// taken to be the same as the filename
+function testTemplateLoad1(test) {
+    const testTemplate = template.create(testFile);
+    const results = tryAllVariableTypes1(testTemplate.load, testVariables);
+
+    const numberOfExpectedSuccesses = 1; // Only string is a valid input.
+    const numberOfExpectedErrors = results.total - numberOfExpectedSuccesses;
+
+    test.expect(6);
+    test.ok(results.errors.length === numberOfExpectedErrors, "Every variable type except string should throw error");
+    test.ok(results.successes.length === numberOfExpectedSuccesses);
+    test.ok(Object.keys(testTemplate.test.blocks).length === 1, "Should have created only one block");
+    test.ok(Object.keys(testTemplate.test.vars).length === 1, "Should have created only one variable");
+    test.ok(testTemplate.test.vars[testVariables.testString1] !== undefined, "variable should correspond to input");
+    test.ok(testTemplate.test.blocks[testVariables.testString1] !== undefined, "block key should correspond to input");
+    test.done();
+}
+// template.load() can also be called with two arguments, the string path to a file, and a string specifying the name
+// of the block within the context of the template, independent of the filename.
+function testTemplateLoad2(test) {
+    const testTemplate = template.create(testFile);
+    const results = tryAllVariableTypes2(testTemplate.load, testVariables);
+
+    const numberOfExpectedSuccesses = 1; // Only a string as both parameters should be valid
+    const numberOfExpectedErrors = results.total - numberOfExpectedSuccesses;
+
+    test.expect(6);
+    test.ok(results.errors.length === numberOfExpectedErrors, "Every variable type except string should throw error");
+    test.ok(results.successes.length === numberOfExpectedSuccesses);
+    test.ok(Object.keys(testTemplate.test.blocks).length === 1, "Should have created only one block");
+    test.ok(Object.keys(testTemplate.test.vars).length === 1, "Should have created only one variable");
+    test.ok(testTemplate.test.vars[testVariables.testString1] !== undefined, "variable should correspond to input");
+    test.ok(testTemplate.test.blocks[testVariables.testString1] !== undefined, "block key should correspond to input");
+    test.done();
+}
+
+function testTemplateSet1(test) {
+    const testTemplate = template.create(testFile);
+    const results = tryAllVariableTypes1(testTemplate.set, testVariables);
+
+    const numberOfExpectedSuccesses = 1; // Only an object literal should be valid
+    const numberOfExpectedErrors = results.total - numberOfExpectedSuccesses;
+
+    test.expect(3);
+    test.ok(results.errors.length === numberOfExpectedErrors, "Only an object literal should be valid");
+    test.ok(results.successes.length === numberOfExpectedSuccesses);
+    test.ok(Object.keys(testTemplate.test.vars).length === testObjectSize, "There are two values in testObject1");
+    test.done();
+}
+
+function testTemplateSet2(test) {
+    const testTemplate = template.create(testFile);
+    const results = tryAllVariableTypes2(testTemplate.set, testVariables);
+
+
+    const numberOfExpectedSuccesses = results.total / Object.keys(testVariables).length;
+    const numberOfExpectedErrors = results.total - numberOfExpectedSuccesses;
+    test.expect(3);
+    test.strictEqual(results.errors.length, numberOfExpectedErrors,
+        "Errors should be thrown if the second parameter is not a string");
+    test.strictEqual(results.successes.length, numberOfExpectedSuccesses);
+    test.strictEqual(Object.keys(testTemplate.test.vars).length, 1, 'only one valid variable should have been created');
+    test.done();
+}
+
+
+
+
+function testPageRender(test) {
+
+    const testTemplate = template.create(testFile);
+
+    testTemplate.set(undefined, "templateTestUndefined1");
+    testTemplate.set(testVariables.testString1, "templateTestString1");
+    testTemplate.set(testVariables.testNumber1, "templateTestNumber1");
+    testTemplate.set(testVariables.testArray1, "templateTestArray1");
+    testTemplate.set(testVariables.testObject1, "templateTestObject1");
+    testTemplate.set(testVariables.testPromise1, "templateTestPromise1");
+    testTemplate.set(testVariables.testReadableStream1, "templateTestReadableStream1");
+    testTemplate.set(testVariables.testDuplexStream1, "templateTestDuplexStream1");
+    testTemplate.set(testVariables.testTransformStream1, "templateTestTransformStream1");
+    testTemplate.set(testVariables.testFunction1, "templateTestFunction1");
+    const templateVarsObject = Object.keys(testTemplate.test.vars);
 
     /**
     * showUndefinedBlock() function testing
     */
     const showUndefinedBlockErrors = [];
-
     const tryShowWithType = function (arg1, arg2) {
         try {
             testTemplate.showUndefinedBlock(arg1, arg2);
@@ -254,14 +340,68 @@ function testPage(test) {
     tryShowWithType(testObject1);
     tryShowWithType(testString1); // The only one that should work
     tryShowWithType(testString1, false);
+    const templateShowTagsObject = Object.keys(testTemplate.test.showTags);
 
-    console.log(testTemplate.test)
+    /**
+    * remove() function testing
+    */
+    const removeErrors = [];
+    const tryRemoveWithType = function (arg1) {
+        try {
+            testTemplate.remove(arg1);
+        } catch (ignore) {
+            removeErrors.push(typeof arg1);
+        }
+    };
+    tryRemoveWithType();
+    tryRemoveWithType(undefined);
+    tryRemoveWithType(null);
+    tryRemoveWithType(testNumber1);
+    tryRemoveWithType(testArray1);
+    tryRemoveWithType(testPromise1);
+    tryRemoveWithType(testReadableStream1);
+    tryRemoveWithType(testDuplexStream1);
+    tryRemoveWithType(testTransformStream1);
+    tryRemoveWithType(testFunction1);
+    tryRemoveWithType(testObject1);
+    tryRemoveWithType(testString1); // The only one that should work
+    const templateHideObject1 = Object.keys(testTemplate.test.hide);
+    const templateHidenBlock1 = testTemplate.test.hide[templateHideObject1[0]];
+
+    /**
+    * retstore() function testing
+    */
+    const restoreErrors = [];
+    const tryRestoreWithType = function (arg1) {
+        try {
+            testTemplate.restore(arg1);
+        } catch (ignore) {
+            restoreErrors.push(typeof arg1);
+        }
+    };
+    tryRestoreWithType();
+    tryRestoreWithType(undefined);
+    tryRestoreWithType(null);
+    tryRestoreWithType(testNumber1);
+    tryRestoreWithType(testArray1);
+    tryRestoreWithType(testPromise1);
+    tryRestoreWithType(testReadableStream1);
+    tryRestoreWithType(testDuplexStream1);
+    tryRestoreWithType(testTransformStream1);
+    tryRestoreWithType(testFunction1);
+    tryRestoreWithType(testObject1);
+    tryRestoreWithType(testString1); // The only one that should work
+    const templateHideObject2 = Object.keys(testTemplate.test.hide);
+    const templateHidenBlock2 = testTemplate.test.hide[templateHideObject2[0]];
+
+    // Visible template test values and setup
 
     testTemplate.showUndefinedBlock("escapedBlock");
-    // Broken into multiple statements because JSLint doesn't like ES6 multi-line template strings
-    let testHtml1 = `<marquee behavior="scroll" direction="left" `;
-    testHtml1 += `style="color:#F00;background-color:#00F;font-size:30px;">`;
-    testHtml1 += `This is some HTML</marquee>`;
+
+    let testHtml1 = `<marquee behavior="scroll" direction="left" `;            // Broken into multiple statements
+    testHtml1 += `style="color:#F00;background-color:#00F;font-size:30px;">`; // because JSLint doesn't like ES6
+    testHtml1 += `This is some HTML</marquee>`;                              // multi-line template strings
+
     testTemplate.set(testHtml1, "templateTestHtml1");
     testTemplate.remove("removedBlock");
     const contactList = [
@@ -304,12 +444,22 @@ function testPage(test) {
                 const $ = dom.load(htmlResult);
 
                 // Expect as many tests as there are elements with the class 'test'
-                const elementCount = $('.test').get().length;
+                const renderTests = $('.test').get().length;
+                const otherTests = 8;
+                test.expect(renderTests + otherTests);
 
-                test.expect(elementCount + 2);
-                test.ok(setErrors.length === 11, "bad set() functions should throw an error");
-                test.ok(showUndefinedBlockErrors.length === 11, "bad showUndefinedBlock() should throw an error");
+                test.ok(setErrors.length === 11, "Bad set() functions should throw an error");
+                test.ok(templateVarsObject.length === 12, 'There should be 12 items in the variables array');
+                test.ok(showUndefinedBlockErrors.length === 11, "Bad showUndefinedBlock() should throw an error");
+                test.ok(templateShowTagsObject.length === 1, "There should be one key in the template showTags object");
+                test.ok(removeErrors.length === 11, "Bad remove() should throw an error");
+                test.ok(templateHideObject1.length === 1, "There should be one key in the template showTags object");
+                test.ok(templateHidenBlock1 === true, "Hidden block should be true");
+                test.ok(restoreErrors.length === 11, "Bad restore() should throw an error");
+                test.ok(templateHideObject2.length === 1, "There should be one key in the template showTags object");
+                test.ok(templateHidenBlock2 === false, "Hidden block should be false");
 
+                // Rendering tests.
                 test.ok($('#testVars1').text().trim() === testString1, "Renders a string");
                 test.ok($('#testVars2').text().trim() === testNumber1.toString(), "Renders a number");
                 test.ok($('#testVars3').text().trim() === JSON.stringify(testArray1), "Renders an array");
@@ -350,7 +500,11 @@ function testPage(test) {
 module.exports = {
     contentParsingTest,
     templateInterfaceTest,
-    invalidTemplateTest,
-    validTemplateTest,
-    testPage
+    testCreateInvalidTemplate,
+    testCreateValidTemplate,
+    testTemplateLoad1,
+    testTemplateLoad2,
+    testTemplateSet1,
+    testTemplateSet2
+    //testPageRender
 };
