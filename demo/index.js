db7553e4f0ff5a1b01b46c1a1c75f2b254d4ef86
Mike Hall <mike.hall@digitaldesignlabs.com>
2016-07-14 10:53:02 +0100

    Make demo work in Node 4

--- demo/index.js
+++ demo/index.js
@@ -1,8 +1,8 @@
 "use strict";
 
-if (process.argv.includes("browser") === true) {
+if (process.argv.indexOf("browser") >= 0) {
     require("./browser");
-} else if (process.argv.includes("console") === true) {
+} else if (process.argv.indexOf("console") >= 0) {
     require("./console");
 } else {
     console.error("Usage: npm run-script demo [type]\n\n   console: console-based demo\n   browser: browser-based demo\n");
